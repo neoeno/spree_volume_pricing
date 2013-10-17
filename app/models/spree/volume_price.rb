4564d625e01023d27a0547f5bf992106910e5337
Caden Lovelace <caden@herostrat.us>
2013-10-17 15:13:43 +0100

    quick fixes for rails 4

--- app/models/spree/volume_price.rb
+++ app/models/spree/volume_price.rb
@@ -7,7 +7,7 @@ class Spree::VolumePrice < ActiveRecord::Base
   validates :range, :format => {:with => /\(?[0-9]+(?:\.{2,3}[0-9]+|\+\)?)/, :message => "must be in one of the following formats: (a..b), (a...b), (a+)"}
   validates :variant, :presence => true
 
-  attr_accessible :amount, :discount_type, :name, :position, :range, :variant
+  # attr_accessible :amount, :discount_type, :name, :position, :range, :variant
 
   OPEN_ENDED = /\(?[0-9]+\+\)?/
 
